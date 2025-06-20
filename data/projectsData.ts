d0cafd953f4492644bc62849bde289cdba483f7c
Vaibhav Bhagat <vaibhavbhagat7461@gmail.com>
2025-06-21 02:22:34 +0530

    Update projectsData.ts

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -14,10 +14,10 @@ export const projectsData: Project[] = [
   },
   {
     id: 'mindsetu',
-    title: 'MindSetu - Mental Wellness Platform for Students',
+    title: 'Mindsetu - Mental Wellness Platform for Students',
     description: '\nBuilt a full-stack web app for college mental health support during my first hackathon. MindSetu features role-based dashboards for students, teachers, and admins; mood tracking; assignment insights; faculty interventions; AI chatbot support; and strict data privacy.',
     stack: ['React', 'Tailwind CSS', 'TypeScript', 'Express', 'Node.js', 'Firebase'],
-    imageUrl: 'https://picsum.photos/seed/webscraper/600/400',
+    imageUrl: 'https://iili.io/FxsR8f1.png',
     demobUrl: 'https://mindsetu-b0db5.web.app/', 
     year: 2023,
   },
