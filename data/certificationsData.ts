aa3b4e6d999db7da8b0ebea9f5495c3f82a9e73e
Vaibhav Bhagat <vaibhavbhagat7461@gmail.com>
2025-06-21 15:41:10 +0530

    Update certificationsData.ts

--- data/certificationsData.ts
+++ data/certificationsData.ts
@@ -7,25 +7,42 @@ export const certificationsData: Certification[] = [
     id: 'ml-a-z-udemy',
     name: 'Machine Learning A-Z™: AI, Python & R + ChatGPT Bonus',
     issuer: 'Udemy',
-    date: 'Ongoing', // Or a completion date like '2024-03'
-    credentialUrl: '#', // Placeholder link
-    icon: UdemyIcon,
+    date: 'January 2025', // Or a completion date like '2024-03'
+    credentialUrl: 'https://www.udemy.com/certificate/UC-189be3bb-1e33-4874-8c89-430de5db2350/', 
+    logUrl: 'https://brandfetch.com/udemy.com?view=library&library=default&collection=logos&asset=idZkl0YU8V&utm_source=https%253A%252F%252Fbrandfetch.com%252Fudemy.com&utm_medium=copyAction&utm_campaign=brandPageReferral',
   },
   {
-    id: 'python-foundations-coursera',
-    name: 'Python for Everybody Specialization',
-    issuer: 'Coursera (University of Michigan)',
-    date: 'Completed - 2023',
-    credentialUrl: '#', // Placeholder link
-    icon: CourseraIcon,
+    id: 'python-data-science',
+    name: 'Python for Data Science',
+    issuer: 'IBM',
+    date: 'July 2023',
+    credentialUrl: 'https://www.credly.com/badges/4fdfb73e-7676-40fe-87fa-8fb638b40cbd', 
+    logoUrl: 'https://static-00.iconduck.com/assets.00/ibm-icon-2048x2048-p5c23kq4.png',
+  },
+  {
+    id: 'foundational-project-management',
+    name: 'Foundations of Project Management',
+    issuer: 'Google',
+    date: 'June 2025',
+    credentialUrl: 'https://www.coursera.org/account/accomplishments/verify/TD8QEM1CH7I6?utm_source%3Dandroid%26utm_medium%3Dcertificate%26utm_content%3Dcert_image%26utm_campaign%3Dsharing_cta%26utm_product%3Dcourse', 
+    logoUrl: 'https://brandfetch.com/google.com?view=library&library=default&collection=logos&asset=idTH30KyjO&utm_source=https%253A%252F%252Fbrandfetch.com%252Fgoogle.com&utm_medium=copyAction&utm_campaign=brandPageReferral',
+  },
+  {
+    id: 'sql-intermediate',
+    name: 'SQL Intermediate Certification',
+    issuer: 'HackerRank',
+    date: 'July 2023',
+    credentialUrl: 'https://www.hackerrank.com/certificates/251aca2f6583', 
+    logoUrl: 'https://brandfetch.com/google.com?view=library&library=default&collection=logos&asset=idTH30KyjO&utm_source=https%253A%252F%252Fbrandfetch.com%252Fgoogle.com&utm_medium=copyAction&utm_campaign=brandPageReferral',
   },
   {
     id: 'web-dev-bootcamp',
     name: 'The Complete Web Development Bootcamp',
     issuer: 'Udemy',
     date: 'Ongoing',
-    credentialUrl: '#', // Placeholder link
-    icon: UdemyIcon,
+    // credentialUrl: '#', 
+    logoUrl: 'https://upload.wikimedia.org/wikipedia/commons/4/40/HackerRank_Icon-1000px.png',
   },
-  // Add more certifications as Vaibhav completes them
+  
+  
 ];
